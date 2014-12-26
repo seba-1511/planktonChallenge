30ffc3570c5d7068b5d6d3283f250c67b2c3adea
seba-1511 <seba-1511@hotmail.com>
2014-12-26 01:21:35 +0100

    Implemented creation of data.

--- code/data/data.py
+++ code/data/data.py
@@ -1,12 +1,36 @@
 # -*- coding: utf-8 -*-
 
+import os
+import cPickle as pickle
+import numpy as np
+
+from skimage.io import imread
 from skimage.transform import resize
 
 
 def create_thumbnail(size=25, img=None):
     if img:
         return resize(img, (size, size))
-    print 'test'
+    folders = os.walk('../../data/train/')
+    images = []
+    classes = []
+    targets = []
+    for class_id, folder in enumerate(folders):
+        classes.append(folder[0][17:])
+        for img in folder[2]:
+            if img.index('.jpg') == -1:
+                continue
+            image = imread(folder[0] + '/' + img)
+            image = resize(image, (size, size))
+            image = np.array(image).ravel()
+            images.append(image)
+            targets.append(class_id)
+    train = {
+        'data': images,
+        'targets': targets
+    }
+    pickle.dump(train, open('train' + str(size) + '.pkl', 'wb'))
+
 
 if __name__ == '__main__':
     create_thumbnail(25)
