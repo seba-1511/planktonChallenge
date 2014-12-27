d5c5b8924cf1a76fd4c905c4b3bd84fa29aa749d
Ubuntu <ubuntu@ip-172-31-19-75.us-west-1.compute.internal>
2014-12-27 14:12:52 +0000

    Implementeation of working net on server.

--- code/main.py
+++ code/main.py
@@ -2,6 +2,7 @@
 
 import warnings
 import numpy as np
+import cPickle as pk
 
 from data.data import Data
 from classifier.kmeans import KMeans
@@ -12,8 +13,9 @@ warnings.filterwarnings("ignore")
 
 NB_CLUSTERS = 20
 
-CLASS_NAMES = ('Protists', 'Crustaceans', 'PelagicTunicates', 'Artifacts', 'Chaetognaths', 'Planktons', 'Copepods', 'Ctenophores', 'ShrimpLike', 'Detritus', 'Diotoms', 'Echinoderms', 'GelatinousZoolankton', 'Fish', 'Gastropods', 'Hydromedusae', 'InvertebrateLarvae', 'Siphonophores', 'Trichodesmium', 'Unknowns')
- 
+CLASS_NAMES = ('Protists', 'Crustaceans', 'PelagicTunicates', 'Artifacts', 'Chaetognaths', 'Planktons', 'Copepods', 'Ctenophores', 'ShrimpLike', 'Detritus',
+               'Diotoms', 'Echinoderms', 'GelatinousZoolankton', 'Fish', 'Gastropods', 'Hydromedusae', 'InvertebrateLarvae', 'Siphonophores', 'Trichodesmium', 'Unknowns')
+
 
 if __name__ == '__main__':
 
@@ -22,29 +24,34 @@ if __name__ == '__main__':
     d.create_categories()
     cnns = []
     for name in CLASS_NAMES:
+        print 'Training for ' + name
         train_X = d.train_cat_X[name]
         train_y = d.train_cat_Y[name]
         test_X = d.test_cat_X[name]
         test_y = d.test_cat_Y[name]
         cnn = CNN(
-            alpha=1, 
-            batch_size=1, 
-            train_X=train_X, 
+            alpha=1,
+            batch_size=1,
+            train_X=train_X,
             train_Y=train_y,
             test_X=test_X,
             test_Y=test_y,
             epochs=200,
             instance_id=121).train()
+        cnns.append[cnn]
         predictions = []
-        for X in d2.test_X:
+        for X in test_X:
             predictions.append(cnn.predict([X, ]))
-        print 'Score for ' + name + ': ' + str(online_score(predictions, d2.valid_Y))
-    
-    
+        print 'Score for ' + name + ': ' + str(online_score(predictions, test_y))
+        pk.dump(cnn, open('cnn_' + name + '.pl', 'wb'))
+    pk.dump(cnns, open('cnns.pl', 'wb'))
+
+
+
     # kclf = KMeans(clusters=NB_CLUSTERS - 1).train(d.train_X)
 
-    # # TODO: Is it a good idea to train the CNN based on data that the Kmeans
-    # # classifies without having seen it ?
+    # TODO: Is it a good idea to train the CNN based on data that the Kmeans
+    # classifies without having seen it ?
     # print 'creating the datasets'
     # train_X = [[] for i in xrange(NB_CLUSTERS)]
     # train_Y = [[] for i in xrange(NB_CLUSTERS)]
@@ -53,12 +60,12 @@ if __name__ == '__main__':
     #     train_X[idx].append(X)
     #     train_Y[idx].append(y)
 
-    # # creating a CNN for each
+    # creating a CNN for each
     # print 'creating and training the cnn'
     # cnns = [CNN(batch_size=1, train_X=train_X[i], train_Y=train_Y[
     #             i], epochs=20).train()for i in xrange(NB_CLUSTERS)]
 
-    # # Benchmark the algo:
+    # Benchmark the algo:
     # print 'benchmarking the algorithm'
     # predictions = []
     # for X in d2.valid_X:
