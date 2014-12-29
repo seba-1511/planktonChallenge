274460791f712742b274b6d8c6600993988a06d3
seba-1511 <seba-1511@hotmail.com>
2014-12-29 15:05:03 +0100

    Cleaned some code.

--- code/main.py
+++ code/main.py
@@ -20,6 +20,31 @@ NB_CLUSTERS = 20
 CLASS_NAMES = ('Protists', 'Crustaceans', 'PelagicTunicates', 'Artifacts', 'Chaetognaths', 'Planktons', 'Copepods', 'Ctenophores', 'ShrimpLike', 'Detritus',
                'Diotoms', 'Echinoderms', 'GelatinousZoolankton', 'Fish', 'Gastropods', 'Hydromedusae', 'InvertebrateLarvae', 'Siphonophores', 'Trichodesmium', 'Unknowns')
 
+def train_steroids_knn(d=None):
+     kclf = KMeans(clusters=NB_CLUSTERS - 1).train(d.train_X)
+
+     #TODO: Is it a good idea to train the CNN based on data that the Kmeans
+     classifies without having seen it ?
+     print 'creating the datasets'
+     train_X = [[] for i in xrange(NB_CLUSTERS)]
+     train_Y = [[] for i in xrange(NB_CLUSTERS)]
+     for X, y in zip(d2.train_X, d2.train_Y):
+     idx = kclf.predict(X)
+     train_X[idx].append(X)
+     train_Y[idx].append(y)
+
+     creating a CNN for each
+     print 'creating and training the cnn'
+     cnns = [CNN(batch_size=1, train_X=train_X[i], train_Y=train_Y[
+     i], epochs=20).train()for i in xrange(NB_CLUSTERS)]
+
+     Benchmark the algo:
+     print 'benchmarking the algorithm'
+     predictions = []
+     for X in d2.valid_X:
+     idx = kclf.predict(X)
+     predictions.append(cnns[idx].predict([X, ]))
+     print online_score(predictions, d2.valid_Y)
 
 def train_specialists(d=None):
     d.create_categories()
@@ -79,29 +104,3 @@ if __name__ == '__main__':
     #train_specialists(d=d)
     train_general(d=d)
 
-
-
-    # kclf = KMeans(clusters=NB_CLUSTERS - 1).train(d.train_X)
-
-    # TODO: Is it a good idea to train the CNN based on data that the Kmeans
-    # classifies without having seen it ?
-    # print 'creating the datasets'
-    # train_X = [[] for i in xrange(NB_CLUSTERS)]
-    # train_Y = [[] for i in xrange(NB_CLUSTERS)]
-    # for X, y in zip(d2.train_X, d2.train_Y):
-    #     idx = kclf.predict(X)
-    #     train_X[idx].append(X)
-    #     train_Y[idx].append(y)
-
-    # creating a CNN for each
-    # print 'creating and training the cnn'
-    # cnns = [CNN(batch_size=1, train_X=train_X[i], train_Y=train_Y[
-    #             i], epochs=20).train()for i in xrange(NB_CLUSTERS)]
-
-    # Benchmark the algo:
-    # print 'benchmarking the algorithm'
-    # predictions = []
-    # for X in d2.valid_X:
-    #     idx = kclf.predict(X)
-    #     predictions.append(cnns[idx].predict([X, ]))
-    # print online_score(predictions, d2.valid_Y)
