756bc0760a72424dae355a4368434c3879e9d16f
Ubuntu <ubuntu@ip-172-31-27-106.us-west-1.compute.internal>
2015-01-22 09:13:19 +0000

    Added additional statistics to main as well.

--- code/main.py
+++ code/main.py
@@ -244,11 +244,19 @@ def train_pylearn_general(d=None):
         print 'Evaluating...'
         predictions = [predict([f, ])[0] for f in train_X[:2000]]
         print np.min(predictions), np.max(predictions)
+        unique, counts = np.unique(predictions, return_counts=True)
+        print 'Count of train predictions: ', (unique, counts)
+        unique, counts = np.unique(train_y[:2000], return_counts=True)
+        print 'Count of train labels: ', (unique, counts)
         print 'Logloss on train: ' + str(online_score(predictions, train_y))
         predictions = [predict([f, ])[0] for f in test_X]
         predictions = predict(test_X)
         print np.min(predictions), np.max(predictions)
         score = online_score(predictions, test_y)
+        unique, counts = np.unique(predictions, return_counts=True)
+        print 'Count of test predictions: ', (unique, counts)
+        unique, counts = np.unique(test_y, return_counts=True)
+        print 'Count of test labels: ', (unique, counts)
         print 'Logloss on test: ' + str(score)
         best, best_iter = (best, best_iter) if best < score else (score, i)
         print 'Current best: ' + str(best) + ' at iter ' + str(best_iter)
