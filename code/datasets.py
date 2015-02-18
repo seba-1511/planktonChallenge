11d4c3175eb142885fb1cda051b0047e113c4dfa
seba-1511 <seba-1511@hotmail.com>
2015-02-18 03:06:38 +0000

    Datasets reads images if they do not exist yet.

--- code/datasets.py
+++ code/datasets.py
@@ -4,27 +4,32 @@ import gzip
 import cPickle as pk
 from data.data import Data, RotationalDDM
 from pylearn2.datasets.dense_design_matrix import DenseDesignMatrix
+from os.path import isfile
 
 from utils import (
-    convert_one_hot
-)
+        convert_one_hot
+        )
 
 #   mnist = fetch_mldata('MNIST original')
-# debug()
-#    mnist.data = (mnist.data.astype(float) / 255)
-# data = Data(size=IMG_SIZE, train_perc=0.8, valid_perc=0.1, test_perc=0.1)
-# f = gzip.open('data.pkl.gz', 'wb')
-# pk.dump(data, f, protocol=pk.HIGHEST_PROTOCOL)
-# f.close()
-# train()
-#
+#   mnist.data = (mnist.data.astype(float) / 255)
 
+
+DATA_FILE = 'data.pkl.gz'
 
 def get_dataset():
     print 'Loading Data...'
-    f = gzip.open('data.pkl.gz', 'rb')
-    d = pk.load(f)
-    f.close()
+    if isfile(DATA_FILE):
+        print 'Loading from gzip...'
+        f = gzip.open(DATA_FILE, 'rb')
+        d = pk.load(f)
+        f.close()
+    else:
+        print 'Reading images...'
+        d = Data(size=IMG_SIZE, train_perc=0.8, valid_perc=0.1, test_perc=0.1)
+        f = gzip.open(DATA_FILE, 'wb')
+        pk.dump(d, f, protocol=pk.HIGHEST_PROTOCOL)
+        f.close()
+
     # train = RotationalDDM(X=d.train_X, y=convert_one_hot(d.train_Y))
     # valid = RotationalDDM(X=d.valid_X, y=convert_one_hot(d.valid_Y))
     # test = RotationalDDM(X=d.test_X, y=convert_one_hot(d.test_Y))
